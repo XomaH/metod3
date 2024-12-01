2aa747a89c4946fc12e2e915650e875ad9b3bb79
XomaH <kympol40@gmail.com>
2024-12-01 08:58:34 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
 
+
         checkingTheYea(2020);
         needingApdateOs(2015, 1);
         System.out.println(calcDeliveryDay(40));
